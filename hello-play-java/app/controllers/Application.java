c9e868df7f9cec0d9623495c8e51577b738a9efa
yenyen <aurelienthevoz@gmail.com>
2013-12-22 14:29:48 +0100

    Add cache to homepage

--- hello-play-java/app/controllers/Application.java
+++ hello-play-java/app/controllers/Application.java
@@ -2,9 +2,11 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.cache.Cache;
 
 public class Application extends Controller {
     
+    @Cached(key = "homePage")
     public static Result index() {
         return ok(views.html.index.render("Hello Play Framework"));
     }
