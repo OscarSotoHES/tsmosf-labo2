1269a5947a81598496944ab4aadff5e96f702d13
yenyen <aurelienthevoz@gmail.com>
2014-01-05 11:31:55 +0100

    Create StudentControllerCache.java

---
+++ SchoolAPI/app/controllers/StudentControllerCache.java
@@ -0,0 +1,100 @@
+package controllers;
+import java.util.concurrent.Callable;
+import java.util.List;
+import play.libs.Json;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.JsonNode;
+import play.*;
+import play.mvc.*;
+import models.*;
+import views.html.*;
+import play.cache.Cache;
+
+public class StudentControllerCache extends Controller {
+
+        public static Result getAll() {
+                try
+                {
+                        List<Student> students;
+                        Callable<List<Student>> call = new Callable<List<Student>>(){
+                                @Override
+                                public List<Student> call() throws Exception {
+                                        return Student.find.all();
+                                }
+                        };
+                        students = Cache.getOrElse("students", call, 10000);
+                        if(students == null)
+                                students = call.call();
+
+                        return ok(Json.toJson(students));
+                } catch (Exception ex) {
+                        System.out.println("Exception in list(): " + ex);
+                        return null;
+                }
+        }
+        
+        public static Result get(final long id) {
+                 try {
+                        Student s = Cache.getOrElse("student" + id, new Callable<Student>() {
+                                @Override
+                                public Student call() throws Exception {
+                                        return Student.find.byId(id);
+                                }
+                        }, 10000);
+                        return ok(Json.toJson(s));
+                } catch (Exception ex) {
+                        System.out.println("Exception in get(" + id + ")" + ex);
+                        return null;
+                }
+        }
+        
+        private static void addToCache(Student student) {
+                Cache.remove("students");
+                Cache.set("student" + student.id, student);
+        }
+
+        public static Result create() {
+                JsonNode json = request().body().asJson();
+                Student student  = Json.fromJson(json, Student.class);
+                student.save();
+                addToCache(student);
+                return ok(Json.toJson(student));
+        }
+
+        public static Result update(long id) {
+                JsonNode json = request().body().asJson();
+                Student student = Json.fromJson(json, Student.class);
+                student.id = id;
+                boolean end = false;
+                int i = 0;
+                while(!end)
+                { 
+                        try
+                        {
+                                student.update();
+                                end = true;
+                        }
+                        catch(Exception ex)
+                        {
+                                try {
+                                            Thread.sleep(100);
+                                } catch (InterruptedException e) {
+                                            e.printStackTrace();
+                                }
+
+                                if(i > 100)
+                                        throw ex;
+                                else
+                                        i++;
+                        }
+                }
+                addToCache(student);
+                return ok(Json.toJson(student));
+        }
+
+        public static Result delete(long id) {
+                Cache.remove("student" + id);
+                Lesson.find.ref(id).delete();
+                return ok();
+        }
+}
